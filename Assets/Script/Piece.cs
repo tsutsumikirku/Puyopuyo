a5146b2b7e92e1cb91ad393caf4a69b6dfb05f68
TsutsumiKiriku <tsutsumikiriku@gmail.com>
2026-02-05 21:41:42 +0900

    Fix UI board placement and sizing

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -1,24 +1,89 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Piece : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    [SerializeField] private PieceType pieceType;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Image uiImage;
+    [SerializeField] private RectTransform rectTransform;
 
-	public void OnDrag(PointerEventData eventData)
+    public PieceType Type => pieceType;
+    public bool IsUI => uiImage != null;
+    public RectTransform RectTransform => rectTransform;
+
+    private void Awake()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (uiImage == null)
+        {
+            uiImage = GetComponent<Image>();
+        }
+
+        if (rectTransform == null)
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+    }
+
+    public void Initialize(PieceType type, Sprite sprite)
+    {
+        pieceType = type;
+        ApplySprite(sprite);
+    }
+
+    public void ApplySprite(Sprite sprite)
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = sprite;
+        }
+
+        if (uiImage != null)
+        {
+            uiImage.sprite = sprite;
+        }
+    }
+
+    public void ApplyUISize(Vector2 size)
+    {
+        if (rectTransform != null)
+        {
+            rectTransform.localScale = Vector3.one;
+            rectTransform.sizeDelta = size;
+        }
+    }
+
+    public void ApplyUIPosition(Vector2 anchoredPosition)
+    {
+        if (rectTransform != null)
+        {
+            rectTransform.anchoredPosition = anchoredPosition;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
     {
     }
 
-	public void OnBeginDrag(PointerEventData eventData)
-	{
-	}
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
 
-	public void OnEndDrag(PointerEventData eventData)
-	{
-	}
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
 }
 public enum PieceType
 {
-    Corner,
-    Edge,
-    Center
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    Purple
 }
